6a1c81c31506c3f2022494694d64cbb21f648241
s-poveda <spoveda545@gmail.com>
2020-09-16 16:32:10 -0400

    added correct prop

--- questions.js
+++ questions.js
@@ -1,20 +1,26 @@
 export default [
     {   text:"If a woodchuck could chuck wood, how much wood would a woodchuck chuck?",
-        answers:["Yes", "No", "10.83 Kilograms", "As much as a woodchuck that could chuck wood should chuck wood."]
+        answers:["Yes", "No", "10.83 Kilograms", "As much as a woodchuck that could chuck wood should chuck wood."],
+        correctIndex: 3
         },
     
     {   text:"What happens when you click this question?",
-        answers:["You fail.", "You win.", "Nothing.", "We create replayability."]
+        answers:["You fail.", "You win.", "Nothing.", "We create replayability."],
+        correctIndex: 0
        },
         
     {   text:"What is the number of this question?",
-        answers:["1", "2", "4", "null?"]
+        answers:["1", "2", "4", "null?"],
+        correctIndex: 3
         },
         
     {   text:"Rich lady lets poor man freeze to death.",
-        answers:[ "Breakfast at Tiffany's", "Who Killed Roger Rabbit?", "Titanic", "Rocky"]
+        answers:[ "Breakfast at Tiffany's", "Who Killed Roger Rabbit?", "Titanic", "Rocky"],
+        correctIndex: 2
         },
     
-    {    text:"British man is not British, and not a man. Still manages lure women in to his blue police box.",
-        answers:["Doctor Who", "CSI: Miami", "Two and a Half Men", "Fox News"]}
+    {   text:"British man is not British, and not a man. Still manages lure women in to his blue police box.",
+        answers:["Doctor Who", "CSI: Miami", "Two and a Half Men", "Fox News"],
+        correctIndex:0 
+        }
     ];

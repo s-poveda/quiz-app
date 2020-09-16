7d79c5740ec7b5386a39f29f5c8380178c52954c
s-poveda <spoveda545@gmail.com>
2020-09-16 11:22:48 -0400

    wireframe

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 /**
  * Example store structure
  */
+import questions from './questions';
 const store = {
   // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
+  questions,
   quizStarted: false,
   questionNumber: 0,
   score: 0

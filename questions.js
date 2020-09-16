ab7e94856c2b7e60e4ea4edd70f119a5013107f7
s-poveda <spoveda545@gmail.com>
2020-09-16 15:54:50 -0400

    reformatted question objects

--- questions.js
+++ questions.js
@@ -1,21 +1,20 @@
 export default [
-    {q1: {   text:"If a woodchuck could chuck wood, how much wood would a woodchuck chuck?",
-            answers:["Yes", "No", "10.83 Kilograms", "As much as a woodchuck that could chuck wood should chuck wood."]
-        }},
+    {   text:"If a woodchuck could chuck wood, how much wood would a woodchuck chuck?",
+        answers:["Yes", "No", "10.83 Kilograms", "As much as a woodchuck that could chuck wood should chuck wood."]
+        },
     
-    {q2:{   text:"What happens when you click this question?",
-           answers:["You fail.", "You win.", "Nothing.", "We create replayability."]
-       }},
+    {   text:"What happens when you click this question?",
+        answers:["You fail.", "You win.", "Nothing.", "We create replayability."]
+       },
         
-    {q3:{    text:"What is the number of this question?",
-            answers:["1", "2", "4", "null?"]
-        }},
+    {   text:"What is the number of this question?",
+        answers:["1", "2", "4", "null?"]
+        },
         
-    {q4:{    text:"Rich lady lets poor man freeze to death.",
-            answers:[ "Breakfast at Tiffany's", "Who Killed Roger Rabbit?", "Titanic", "Rocky"]
-        }},
+    {   text:"Rich lady lets poor man freeze to death.",
+        answers:[ "Breakfast at Tiffany's", "Who Killed Roger Rabbit?", "Titanic", "Rocky"]
+        },
     
-    {q5:{    text:"British man is not British, and not a man. Still manages lure women in to his blue police box.",
-            answers:["Doctor Who", "CSI: Miami", "Two and a Half Men", "Fox News"]}}
+    {    text:"British man is not British, and not a man. Still manages lure women in to his blue police box.",
+        answers:["Doctor Who", "CSI: Miami", "Two and a Half Men", "Fox News"]}
     ];
-// question[0].q1.answer

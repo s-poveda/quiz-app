10d1f9f4fdbfe02798a7f2315080835a7b7b8bfd
s-poveda <spoveda545@gmail.com>
2020-09-16 17:58:27 -0400

    wip

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
  * Example store structure
  */
 
-import questions from './questions.js';
+import questions from "./questions.js";
 
 const store = {
   // 5 or more questions are required
   questions,
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
 };
 
 function createStartScreen() {
@@ -17,44 +17,68 @@ function createStartScreen() {
 }
 
 function createQuestionScreen() {
-  return `<div class="ui grid container">
-  <h2 class="centered" id="question">${store.questions[store.questionNumber - 1].text}</h2>
+  const questionContainer = store.questions[store.questionNumber - 1];
+  return `
+  <div class="ui grid container">
+  <h2 class="ui centered fourteen row" id="question">${questionContainer.text}</h2>
   <form action="" class="ui twelve wide column segment form centered">
     <!-- <img src="https://semantic-ui.com/images/avatar2/large/kristy.png" alt="" class="ui card centered"> -->
-    <input type="radio" id="answer">answer button</button>
+    <div class="ui" id="answer-list">
+    ${questionContainer.answers
+      .map((answer, index) => {
+        return `
+      <div class="ui">
+          <input class="ui input" type="radio" name="answer" id=answer data-item-index="${index}">
+          <label for="answer" class="answer-text">${answer}</label>
+      </div>
+      
+      `;
+      })
+      .join("")}
+  </div>
+  <button class="ui button right floated" id="next">Next</button>
   </form>
-</div>`
+</div>
+`;
 }
 
 function createHeader() {
-// TODO: change the name of quiz
-return `
+  // TODO: change the name of quiz
+  return `
 <h1 class="ui header">NAME OF QUIZ</h1>
-<h2 class="active item" id="question-number"> Question: ${store.questionNumber} of ${store.questions.length}</h2>
+<h2 class="active item" id="question-number"> Question: ${store.questionNumber}/${store.questions.length}</h2>
 <h2 class="item">${store.score} / ${store.questions.length}</h2>
-`
+`;
 }
 
-function createResultsScreen () {
-console.log('result screen');
+function createResultsScreen() {
+  console.log("result screen");
 }
 
-
-
-function handleStartClick () {
-  const main = $('main');
-  $('main').on('click', '#start-button', (e) => {
+function handleStartClick() {
+  const main = $("main");
+  $("main").on("click", "#start-button", (e) => {
     store.questionNumber = 1;
     render();
   });
 }
 
-function handleAnswerClick () {
-  $('main').on('click', '#answer', (e) => {
-    // console.log(e);
+function handleAnswerClick() {
+  $("main").on("click", "#next", (e) => {
+    e.preventDefault();
+    console.log('in Answer click', e);
+    store.quizStarted = true;
+
+    //gets current 
+    const questionContainer = store.questions[store.questionNumber - 1];
+    const selectedAnswer = $('main').find('input[name="answer"]');
+    console.log(selectedAnswer);
     store.questionNumber++;
+
+    // if (questionContainer.correct == )
+    store.score++;
     render();
-  })
+  });
 }
 
 function handleRetryClick() {
@@ -65,40 +89,38 @@ function handleRetryClick() {
 }
 
 function render() {
-
-  const main = $('main');
-  const header = $('header');
+  const main = $("main");
+  const header = $("header");
   console.log(store.quizStarted == true);
   if (store.questionNumber === 0) {
-    console.log('in store.score');
+    console.log("in store.score");
     if (!store.quizStarted) return main.html(createStartScreen());
-      console.log('after quiz started');
-     return main.html(createQuestionScreen());
+    console.log("after quiz started");
+    return main.html(createQuestionScreen());
   }
 
   if (store.questionNumber == store.questions.length) {
-    console.log('questions ended');
+    console.log("questions ended");
     header.empty();
-    return main.html(createResultsScreen());    
+    return main.html(createResultsScreen());
   }
   console.log(`Current question: ${store.questionNumber}`);
   header.html(createHeader());
   main.html(createQuestionScreen());
 }
 
-$( () => {
+$(() => {
   handleRetryClick();
   handleAnswerClick();
   handleStartClick();
-  render(); 
-  }
-);
+  render();
+});
 
 /**
- * 
+ *
  * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ *
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
  * See your course material and access support for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
@@ -106,8 +128,8 @@ $( () => {
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- * 
+ *
+ *
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
